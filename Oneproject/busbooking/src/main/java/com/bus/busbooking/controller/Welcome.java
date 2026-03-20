92e5faac0ad9531180b8c60c1e57ed94fb54f4d8
guruvardhan-tech-village <guruvardhaniniot@gmail.com>
2026-03-20 11:51:45 +0530

    spring

--- Oneproject/busbooking/src/main/java/com/bus/busbooking/controller/Welcome.java
+++ Oneproject/busbooking/src/main/java/com/bus/busbooking/controller/Welcome.java
@@ -5,11 +5,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+import com.bus.busbooking.model.User;
 
 @RestController         //tells to spring that this class is a controller and it will handle the incoming requests
 
 
+
 public class Welcome {
+
+    private List<User> userList = new ArrayList<>();
+
     @GetMapping("/welcome")  //(@getmapping is used to handle GET requests) when user hits the /welcome endpoint, this method will be called and it will return the welcome message 
     public String welcome() {
         return "Welcome to the Bus Booking System! 🚍";
@@ -23,9 +30,22 @@ public class Welcome {
         return "Hello " + name;
     }
 
-    @PostMapping("/addUser") //@PostMapping is used to handle POST requests. When a user sends a POST request to the /addUser endpoint with a name in the request body, this method will be called. The @RequestBody annotation is used to extract the value of the name parameter from the body of the POST request. For example, if the user sends a POST request to /addUser with a JSON body like {"name": "John"}, the value of name will be "John". The method then returns a message confirming that the user has been added successfully.
-    public String addUser(@RequestBody String name) {  //@RequestBody is used to extract the value of the name parameter from the body of the POST request. For example, if the user sends a POST request to /addUser with a JSON body like {"name": "John"}, the value of name will be "John". The method then returns a message confirming that the user has been added successfully.
-        return "User " + name + " added successfully";
+    // @PostMapping("/addUser") //@PostMapping is used to handle POST requests. When a user sends a POST request to the /addUser endpoint with a name in the request body, this method will be called. The @RequestBody annotation is used to extract the value of the name parameter from the body of the POST request. For example, if the user sends a POST request to /addUser with a JSON body like {"name": "John"}, the value of name will be "John". The method then returns a message confirming that the user has been added successfully.
+    // public String addUser(@RequestBody String name) {  //@RequestBody is used to extract the value of the name parameter from the body of the POST request. For example, if the user sends a POST request to /addUser with a JSON body like {"name": "John"}, the value of name will be "John". The method then returns a message confirming that the user has been added successfully.
+    //     return "User " + name + " added successfully";
+    // }
+
+    @PostMapping("/addUser")
+    public String addUser(@RequestBody User user) {
+        userList.add(user);
+        return "User added successfully";
     }
+
+    @GetMapping("/getAllUsers")
+    public List<User> getAllUsers() {
+        return userList;
+    }
+    
+    
 
 }
---
+++ busdatabse.mongodb.js
@@ -0,0 +1,3 @@
+use busdb
+
+db.createCollection("buses")
