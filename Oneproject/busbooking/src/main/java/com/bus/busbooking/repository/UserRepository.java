cf7992187fd8dd8386c93e6092b33b7e8b0cae94
guruvardhan-tech-village <guruvardhaniniot@gmail.com>
2026-03-23 15:32:06 +0530

    resolving problems

--- Oneproject/busbooking/src/main/java/com/bus/busbooking/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bus.busbooking.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.bus.busbooking.model.User;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
