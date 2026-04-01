f9e86292da714202ed199c18cc433f55466b774e
guruvardhan-tech-village <guruvardhaniniot@gmail.com>
2026-04-01 12:12:19 +0530

    ..

---
+++ Backend/AI_Ecommerce(SQL)/ecommerce/src/main/java/com/ai_ecommerce/ecommerce/model/Products.java
@@ -0,0 +1,70 @@
+package com.ai_ecommerce.ecommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+    private String companyName;
+    private String category;
+
+    public Products() {
+    }
+
+    public Products(String name, String description, Double price, String companyName, String category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.companyName = companyName;
+        this.category = category;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public String getCompanyName() {
+        return companyName;
+    }
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+
+}
