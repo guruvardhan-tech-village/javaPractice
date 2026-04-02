f8ec5b213f614eec1f8cc79ce64c9f5b71373b2f
guruvardhan-tech-village <guruvardhaniniot@gmail.com>
2026-04-02 09:47:54 +0530

    controlluodate

--- Backend/AI_Ecommerce(SQL)/ecommerce/src/main/java/com/ai_ecommerce/ecommerce/controller/ProductController.java
+++ Backend/AI_Ecommerce(SQL)/ecommerce/src/main/java/com/ai_ecommerce/ecommerce/controller/ProductController.java
@@ -3,13 +3,13 @@ package com.ai_ecommerce.ecommerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.ai_ecommerce.ecommerce.model.Products;
 import com.ai_ecommerce.ecommerce.service.ProductService;
 
@@ -28,9 +28,9 @@ public class ProductController {
     public List<Products> getAllproducts(){
         return productService.getAllProducts();
     }
-    @GetMapping("/getProductsByCompanyName")
-    public List<Products> getProductsByCompanyName(@RequestParam String companyName){
-        return productService.getProductsByCompanyName(companyName);
+    @GetMapping("/getByCompanyName")
+    public List<Products> getByCompanyName(@RequestParam String companyName){
+        return productService.findByCompanyName(companyName);
     }
     @GetMapping("/getProductsByPriceRange")
     public List<Products> getProductsByPriceRange(@RequestParam double min, @RequestParam double max){
