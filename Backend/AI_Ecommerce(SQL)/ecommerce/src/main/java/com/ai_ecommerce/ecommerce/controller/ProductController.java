08c277c5d211a274cabb59c83c64fd0c9a9759cb
guruvardhan-tech-village <guruvardhaniniot@gmail.com>
2026-04-02 10:01:10 +0530

    update

--- Backend/AI_Ecommerce(SQL)/ecommerce/src/main/java/com/ai_ecommerce/ecommerce/controller/ProductController.java
+++ Backend/AI_Ecommerce(SQL)/ecommerce/src/main/java/com/ai_ecommerce/ecommerce/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.ai_ecommerce.ecommerce.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,25 +13,29 @@ import com.ai_ecommerce.ecommerce.model.Products;
 import com.ai_ecommerce.ecommerce.service.ProductService;
 
 @RestController
-@RequestMapping("products")
+@RequestMapping("/products")
 public class ProductController {
-    @Autowired
-    private ProductService productService;
+    
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @PostMapping("/addProducts")
     public String addProducts(@RequestBody Products products){
         return productService.addProducts(products);
     }
 
-    @GetMapping("getAllProducts")
+    @GetMapping("/getAllProducts")
     public List<Products> getAllproducts(){
         return productService.getAllProducts();
     }
-    @GetMapping("/getByCompanyName")
-    public List<Products> getByCompanyName(@RequestParam String companyName){
+    @GetMapping("/company")
+    public List<Products> getProductsByCompanyName(@RequestParam String companyName){
         return productService.findByCompanyName(companyName);
     }
-    @GetMapping("/getProductsByPriceRange")
+    @GetMapping("/price-range")
     public List<Products> getProductsByPriceRange(@RequestParam double min, @RequestParam double max){
         return productService.findByPriceBetween(min, max);
     }
